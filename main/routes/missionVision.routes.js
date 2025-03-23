ee5937c3c955575675b985c774caea922e0b6a36
unknown <dibbu01>
2025-03-24 00:33:41 +0530

    Add new feature

--- main/routes/missionVision.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import { addMissionVision, getMissionVision } from '../controller/missionVision.controller.js';
-import protectroute from '../middleware/protectrouteuser.js';
-
-router.post('/mission-vision',protectroute,addMissionVision);
-router.get('/mission-vision',protectroute,getMissionVision);
-
-export default router;  
